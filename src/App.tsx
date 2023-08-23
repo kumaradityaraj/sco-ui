3582a440ee284d6e7eb58bd4ce6550baea2fdc8b
kumaradityaraj <sedulous.0007@gmail.com>
2023-08-23 19:50:09 +0530

    SCO-20 Create skeleton project for the web UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "@patternfly/react-core/dist/styles/base.css";
+import "@patternfly/patternfly/patternfly.css";
+import Header  from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
